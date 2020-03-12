c8df724d45509b250663175b1dffe5ea40fb1140
Ryan Clarke <ryan.clarke@gov.yk.ca>
2020-03-12 12:39:54 -0700

    refactor a bit and added clearing MAC when invalid

--- contentscript.js
+++ contentscript.js
@@ -16,22 +16,17 @@ chrome.storage.sync.get({
         var current_usage = document.getElementsByTagName("a")[3].text.split(" ")[0];
         // Get maximum usage for plan
         var max_usage = document.getElementsByTagName("td")[1].innerText.split(" ")[0];
-        // Date calculations
-        var date = new Date();
-        var first_day = new Date(date.getFullYear(), date.getMonth(), 1);
-        var last_day = new Date(date.getFullYear(), date.getMonth() + 1, 0);
         // Unix time calculations
-        var unix_now = Math.round(date.getTime()/1000);
-        var unix_start_of_month = Math.round(first_day.getTime()/1000);
-        var unix_end_of_month = Math.round(last_day.getTime()/1000);
+        var today = new Date();
+        var unix_now = Math.round(today.getTime()/1000);
+        var unix_start_of_month = Math.round((new Date(today.getFullYear(), today.getMonth(), 1)).getTime()/1000);
+        var unix_end_of_month = Math.round((new Date(today.getFullYear(), today.getMonth() + 1, 0)).getTime()/1000);
         // Months in seconds calculations
         var seconds_in_month = unix_end_of_month - unix_start_of_month;
         var seconds_into_month = unix_now - unix_start_of_month;
         var percent_month_completed = seconds_into_month / seconds_in_month;
-        // Calculate expected usage for this point of month
-        var expected_usage = max_usage * percent_month_completed;
         // Positive indicates available cap - negative indicates overusage
-        var usage_difference = (expected_usage - current_usage).toFixed(1);
+        var usage_difference = ((max_usage * percent_month_completed) - current_usage).toFixed(1);
         // Generate usage projection
         var usage_projection = (current_usage / percent_month_completed).toFixed(1);
         // Base style for banner
---
+++ reached_error_page.js
@@ -0,0 +1,3 @@
+chrome.storage.sync.set({
+    mac_address: '',
+  }, function() {});
