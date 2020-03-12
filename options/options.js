3433ab3bb7913a7d8c4e7640c56462a7bae930e5
Ryan Clarke <ryan.clarke@gov.yk.ca>
2020-03-12 13:03:34 -0700

    got rid of jankiness of options

--- options/options.js
+++ options/options.js
@@ -6,24 +6,10 @@ function save_options() {
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
-    status.textContent = 'Options saved. Redirecting to usage site in 5.';
+    status.textContent = 'Options saved.';
     setTimeout(function() {
-      status.textContent = 'Options saved. Redirecting to usage site in 4.';
-      setTimeout(function() {
-        status.textContent = 'Options saved. Redirecting to usage site in 3.';
-        setTimeout(function() {
-          status.textContent = 'Options saved. Redirecting to usage site in 2.';
-          setTimeout(function() {
-            status.textContent = 'Options saved. Redirecting to usage site in 1.';
-            setTimeout(function() {
-              status.textContent = '';
-                var newURL = "https://ubbapps.nwtel.ca/cable_usage/secured/index.jsp";
-                chrome.tabs.create({ url: newURL });
-            }, 1000);
-          }, 1000);
-        }, 1000);
-      }, 1000);
-    }, 1000);
+      status.textContent = '';
+    }, 5000);
   });
 }
 // Restores mac_address state using the preferences stored in chrome.storage.
