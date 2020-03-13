5475ef7eeb6551fac5522f3d98373e4e9272d50c
Ryan Clarke <ryan.clarke@gov.yk.ca>
2020-03-13 10:11:26 -0700

    refactor

--- content_scripts/login_and_calc_usage.js
+++ content_scripts/login_and_calc_usage.js
@@ -1,4 +1,7 @@
 'use strict';
+
+
+
 chrome.storage.sync.get({
     mac_address: ''
   }, function(items) {
@@ -12,10 +15,45 @@ chrome.storage.sync.get({
         // Not on logon page, continue.
     }
     try {
-        // Get current usage for this month
-        var current_usage = document.getElementsByTagName("a")[3].text.split(" ")[0];
-        // Get maximum usage for plan
-        var max_usage = document.getElementsByTagName("td")[1].innerText.split(" ")[0];
+        
+        var month_calc = new UsageMonthCalculator();
+        var usage = new Usage(month_calc.percent_month_completed);
+        var usage_message = new UsageMessage(usage);
+
+        //var usage_text = usage_message.text;
+        var new_element = document.createElement("strong");
+        var element = document.getElementsByTagName("p")[4];
+        new_element.style.cssText = usage_message.style;
+        var node = document.createTextNode(usage_message.text);
+        new_element.appendChild(node);
+        element.appendChild(new_element);
+    } catch(err){}
+});
+
+class UsageMessage {
+    constructor(usage){
+        this.style = 'font-size:1.5em;background-color:LightGrey;border-style: outset;padding: .1em .1em .1em .1em;';
+        var usage_diff_text = "You are " + usage.difference + " GB under your expected usage.";
+        if(usage.difference < 0){
+            usage_diff_text = "You are " + (0 - usage.difference) + " GB over your expected usage.";
+            this.setFontBad();
+        } else {
+            this.setFontGood();
+        }
+        this.text = usage_diff_text + " You are projected to use " + usage.projection + " GB.";
+    }
+
+    setFontGood(){
+        this.style += 'color:green';
+    }
+
+    setFontBad(){
+        this.style += 'color:red';
+    }
+}
+
+class UsageMonthCalculator {
+    constructor(){
         // Unix time calculations
         var today = new Date();
         var unix_now = Math.round(today.getTime()/1000);
@@ -24,29 +62,15 @@ chrome.storage.sync.get({
         // Months in seconds calculations
         var seconds_in_month = unix_end_of_month - unix_start_of_month;
         var seconds_into_month = unix_now - unix_start_of_month;
-        var percent_month_completed = seconds_into_month / seconds_in_month;
-        // Positive indicates available cap - negative indicates overusage
-        var usage_difference = ((max_usage * percent_month_completed) - current_usage).toFixed(1);
-        // Generate usage projection
-        var usage_projection = (current_usage / percent_month_completed).toFixed(1);
-        // Base style for banner
-        var style = 'font-size:1.5em;background-color:LightGrey;border-style: outset;padding: .1em .1em .1em .1em;'
-        // Generate over/under usage difference message, set style based on over or under.
-        var usage_diff_text = "You are " + usage_difference + " GB under your expected usage.";
-        if(usage_difference < 0){
-            usage_diff_text = "You are " + (0 - usage_difference) + " GB over your expected usage.";
-            style += 'color:red'
-        }
-        else {
-            style += 'color:green'
-        }
-        // Add usage text to top of page
-        var usage_text = usage_diff_text + " You are projected to use " + usage_projection + " GB.";
-        var new_element = document.createElement("strong");
-        var element = document.getElementsByTagName("p")[4];
-        new_element.style.cssText = style;
-        var node = document.createTextNode(usage_text);
-        new_element.appendChild(node);
-        element.appendChild(new_element);
-    } catch(err){}
-});
+        this.percent_month_completed = seconds_into_month / seconds_in_month;
+    }
+}
+
+class Usage {
+    constructor(percent_month_completed) {
+        this.max = document.getElementsByTagName("td")[1].innerText.split(" ")[0];
+        this.current = document.getElementsByTagName("a")[3].text.split(" ")[0];
+        this.difference = ((this.max * percent_month_completed) - this.current).toFixed(1);
+        this.projection = (this.current / percent_month_completed).toFixed(1);
+    }
+  }
